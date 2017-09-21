cacc8ea89ff6327518beda43c295eaae2f1c7c31
Akhil Jalan <akhiljalan@berkeley.edu>
2017-09-21 16:09:35 -0700

    reworking bernie.py

--- bernie.py
+++ bernie.py
@@ -1,16 +1,20 @@
+'''An approximation of Ugly God's 'Bernie Sanders.' 
+Please excuse any profanity. '''
+
 import numpy as np 
 
-adlibs = ['skrt skrt', 'swag swag', 'trap trap', 
+begin_lines = ['skrt skrt', 'swag swag', 'trap trap', 
 		  'gang gang', 'wrist game', 'dick game', 'get cash', 'flow like', 'big boss']
 
-endlines = ['(YUH!)', '(QUAVO!)', '(SKRRRT!)', '(UGHH!)', '(BANG BANG!)']
+
+adlibs = ['(YUH!)', '(QUAVO!)', '(SKRRRT!)', '(UGHH!)', '(BANG BANG!)']
 
 def bernie(num_lines): 
 	for _ in range(num_lines): 
-		index = np.random.randint(low=0, high=len(adlibs))
-		line = adlibs[index] + ' bernie sanders '
+		index = np.random.randint(low=0, high=len(begin_lines))
+		line = begin_lines[index] + ' bernie sanders '
 		line = line.title()
 		if (np.random.randint(20) % 3 == 0):
-			endline_index = np.random.randint(low=0, high=len(endlines))
-			line += endlines[endline_index]
+			endline_index = np.random.randint(low=0, high=len(adlibs))
+			line += adlibs[endline_index]
 		print(line)
